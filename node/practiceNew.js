50dcca8ab001043e749387ad5ef384faa1d365b6
Yogesh Yadav <yogesh7132@gmail.com>
2021-02-10 19:00:47 +0530

    Added JS

---
+++ node/practiceNew.js
@@ -0,0 +1,16 @@
+let user = [{name:'ram', age:12, class:5},
+{name:'shyam', age:14, class:7},
+{name:'jitu', age: 11, class:4}]
+
+let d = user.forEach(function (arrVal){
+    
+   
+})
+
+// let i 
+// for (i=0; i < user.length; i++){
+//     console.log(user[i])    
+// }
+
+console.log(d)
+
