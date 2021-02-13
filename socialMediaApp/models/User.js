9a9ed2d1e906ad05d975c8e0900b7a0fc7562959
Yogesh Yadav <yogesh7132@gmail.com>
2021-02-13 17:05:37 +0530

    Added the clean up method before validation

--- socialMediaApp/models/User.js
+++ socialMediaApp/models/User.js
@@ -13,7 +13,21 @@ User.prototype.validate = function(){
     if(this.data.password.length < 6) {this.error.push("password length should be of minimum 6 characters")}
 }
 
+User.prototype.catchUp= function(){
+    if(typeof(this.data.username) != "string") {this.data.username=""}
+    if(typeof(this.data.email) != "string") {this.data.email=""}
+    if(typeof(this.data.password) != "string") {this.data.password=""}
+
+    // getting rid of bogus properties
+    this.data = {
+        username : this.data.username.trim().toLowerCase(),
+        email : this.data.email.trim().toLowerCase(),
+        password : this.data.password
+    }
+}
+
 User.prototype.register = function(){
+    this.catchUp()
     this.validate()
 }
 
