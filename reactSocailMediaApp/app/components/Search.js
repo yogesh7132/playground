523d6d73b7ac8726706c38f54043021e8719847b
Yogesh Yadav <yogesh7132@gmail.com>
2021-04-16 16:24:38 +0530

    Added search typing delay & state using useImmer

--- reactSocailMediaApp/app/components/Search.js
+++ reactSocailMediaApp/app/components/Search.js
@@ -1,19 +1,51 @@
 import React, { useContext, useEffect } from "react"
 import DispatchContext from "../DispatchContext"
+import { useImmer } from "use-immer"
 
 function Search() {
   const appDispatch = useContext(DispatchContext)
+
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0
+  })
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler)
     return document.removeEventListener("keyUp", searchKeyPressHandler)
   }, [])
 
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      setState(draft => {
+        draft.requestCount++
+      })
+    }, 3000)
+    return () => clearTimeout(delay)
+  }, [state.searchTerm])
+
+  useEffect(() => {
+    if (state.requestCount) {
+      //send axios request here
+      console.log(state.searchTerm)
+    }
+  }, [state.requestCount])
+
   function searchKeyPressHandler(e) {
     if (e.keyCode == 27) {
       appDispatch({ type: "closeSearch" })
     }
   }
+
+  function handleInput(e) {
+    const value = e.target.value
+    setState(draft => {
+      draft.searchTerm = value
+    })
+  }
+
   return (
     <div className="search-overlay">
       <div className="search-overlay-top shadow-sm">
@@ -21,7 +53,7 @@ function Search() {
           <label htmlFor="live-search-field" className="search-overlay-icon">
             <i className="fas fa-search"></i>
           </label>
-          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+          <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
           <span onClick={() => appDispatch({ type: "closeSearch" })} className="close-live-search">
             <i className="fas fa-times-circle"></i>
           </span>
