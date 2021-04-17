9a075a7d7a2d287eb9cb151b626472c4f3555fae
Yogesh Yadav <yogesh7132@gmail.com>
2021-04-17 15:29:31 +0530

    Add: live search functionality

--- reactSocailMediaApp/app/components/Search.js
+++ reactSocailMediaApp/app/components/Search.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from "react"
 import DispatchContext from "../DispatchContext"
 import { useImmer } from "use-immer"
+import Axios from "axios"
+import {Link} from "react-router-dom"
 
 function Search() {
   const appDispatch = useContext(DispatchContext)
@@ -14,22 +16,46 @@ function Search() {
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler)
-    return document.removeEventListener("keyUp", searchKeyPressHandler)
+    return () => document.removeEventListener("keyup", searchKeyPressHandler)
   }, [])
 
   useEffect(() => {
-    const delay = setTimeout(() => {
+    if (state.searchTerm.trim()) {
       setState(draft => {
-        draft.requestCount++
+        draft.show = "loading"
       })
-    }, 3000)
-    return () => clearTimeout(delay)
+      const delay = setTimeout(() => {
+        setState(draft => {
+          // console.log(state.searchTerm)
+          draft.requestCount++
+        })
+      }, 750)
+      return () => clearTimeout(delay)
+    } else {
+      setState(draft => {
+        draft.show = "neither"
+      })
+    }
   }, [state.searchTerm])
 
   useEffect(() => {
     if (state.requestCount) {
-      //send axios request here
-      console.log(state.searchTerm)
+      const ourRequest = Axios.CancelToken.source()
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/search", { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token })
+          // console.log("response", response.data)
+          // console.log("response", response.data.length)
+          setState(draft => {
+            draft.results = response.data
+            draft.show = "results"
+          })
+        } catch (e) {
+          console.log("There was a problem or the request was cancelled.")
+        }
+      }
+      fetchResults()
+      return () => ourRequest.cancel()
     }
   }, [state.requestCount])
 
@@ -62,24 +88,26 @@ function Search() {
 
       <div className="search-overlay-bottom">
         <div className="container container--narrow py-3">
-          <div className="live-search-results live-search-results--visible">
-            <div className="list-group shadow-sm">
+          <div className={"circle-loader " + (state.show == "loading" ? "circle-loader--visible" : "")}></div>
+          <div className={"live-search-results " + (state.show == "results" ? "live-search-results--visible" : "")}>
+           {Boolean(state.results.length) && (
+              <div className="list-group shadow-sm">
               <div className="list-group-item active">
-                <strong>Search Results</strong> (3 items found)
+                <strong>Search Results</strong> ({state.results.length} {state.results.length > 1 ? "items" : "item "} found)
               </div>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
-                <span className="text-muted small">by brad on 2/10/2020 </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128" /> <strong>Example Post #2</strong>
-                <span className="text-muted small">by barksalot on 2/10/2020 </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
-                <span className="text-muted small">by brad on 2/10/2020 </span>
-              </a>
+             {state.results.map(post =>{
+               const date = new Date(post.createdDate)
+               const formatedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+               return (
+                 <Link  onClick={()=> appDispatch({type: "closeSearch"})} key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
+                   <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> {""}
+                   <span className="text-muted small">by {post.author.username} on {formatedDate} </span>
+                 </Link>
+               )
+             })}
             </div>
+           )}
+           {Boolean(!state.results.length) && <p className="alert alert-danger text-center shadow-sm">Sorry, we could not find any result for that search.</p>}
           </div>
         </div>
       </div>
