4266023456d2600841b0f089c15f4062f3003307
Yogesh Yadav <yogesh7132@gmail.com>
2021-04-17 15:35:15 +0530

    Fix: mongodb index issue

--- backend-api/models/Post.js
+++ backend-api/models/Post.js
@@ -4,6 +4,8 @@ const ObjectID = require('mongodb').ObjectID
 const User = require('./User')
 const sanitizeHTML = require('sanitize-html')
 
+postsCollection.createIndex({title: "text", body: "text"})
+
 let Post = function(data, userid, requestedPostId) {
   this.data = data
   this.errors = []
@@ -77,7 +79,7 @@ Post.prototype.actuallyUpdate = function() {
   })
 }
 
-Post.reusablePostQuery = function(uniqueOperations, visitorId) {
+Post.reusablePostQuery = function(uniqueOperations, visitorId, finalOperations = []) {
   return new Promise(async function(resolve, reject) {
     let aggOperations = uniqueOperations.concat([
       {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
@@ -88,7 +90,7 @@ Post.reusablePostQuery = function(uniqueOperations, visitorId) {
         authorId: "$author",
         author: {$arrayElemAt: ["$authorDocument", 0]}
       }}
-    ])
+    ]).concat(finalOperations)
 
     let posts = await postsCollection.aggregate(aggOperations).toArray()
 
@@ -155,9 +157,8 @@ Post.search = function(searchTerm) {
   return new Promise(async (resolve, reject) => {
     if (typeof(searchTerm) == "string") {
       let posts = await Post.reusablePostQuery([
-        {$match: {$text: {$search: searchTerm}}},
-        {$sort: {score: {$meta: "textScore"}}}
-      ])
+        {$match: {$text: {$search: searchTerm}}}
+      ], undefined, [{$sort: {score: {$meta: "textScore"}}}])
       resolve(posts)
     } else { 
       reject()
