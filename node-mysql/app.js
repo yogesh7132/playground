59c6e9e7f4f23503294f2fd5f3e4babbab696ff6
Yogesh Yadav <yogesh7132@gmail.com>
2021-05-20 22:59:01 +0530

    node-mysql: initials

---
+++ node-mysql/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require("path")
+const connectDb = require("./db")
+
+// Connecting to the database 
+connectDb()
+
+
+const app =  express()
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname,"views"))
+
+app.get("/",(req,res)=>{
+    res.send("ok ok")
+})
+
+
+app.listen(8080, ()=>{
+    console.log("Server Running at Port 8080")
+})
---
+++ node-mysql/db.js
@@ -0,0 +1,17 @@
+const {Sequelize} = require("sequelize");
+
+const sequelize = new Sequelize('blog_app',"root","password",{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+const connectDb = async()=>{
+    try{
+        await sequelize.authenticate()
+        console.log("Connection Open")
+    }catch(error){
+        console.log("Connection Failed",error)
+    }
+}
+
+module.exports = connectDb
